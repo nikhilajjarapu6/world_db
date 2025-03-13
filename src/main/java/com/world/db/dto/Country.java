7b240271d52ebbcec3d41d7871cb6009422ae907
DELL <DELL@saisingarathi.mshome.net>
2025-03-13 12:39:51 +0530

    created variables and getters,setters

--- src/main/java/com/world/db/dto/Country.java
+++ src/main/java/com/world/db/dto/Country.java
@@ -1,7 +1,29 @@
 package com.world.db.dto;
 
 public class Country {
-	public static void main(String[] args) {
-		System.out.println("hello");
+	private String name;
+	private String population;
+	public Country() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
+	public Country(String name, String population) {
+		super();
+		this.name = name;
+		this.population = population;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPopulation() {
+		return population;
+	}
+	public void setPopulation(String population) {
+		this.population = population;
+	}
+	
+	
 }
