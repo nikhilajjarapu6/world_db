e29513494100855e11982eb3b06750f5567607a2
DELL <DELL@saisingarathi.mshome.net>
2025-03-13 12:31:35 +0530

    First commit

---
+++ src/main/java/com/world/db/WorldDbApplication.java
@@ -0,0 +1,13 @@
+package com.world.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/world/db/WorldDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
